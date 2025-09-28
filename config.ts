ec9f6f6d4ba6148de5472ad77847997bd3b76c83
freelancerzahed <freelancerzahed@gmail.com>
2025-09-28 12:59:48 +0600

    url updated

--- config.ts
+++ config.ts
@@ -1,5 +1,5 @@
 // config.ts
-export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost/mirrormefashion/api/v2"
+export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://mirrormefashion.com/api/v2"
 
 export const recaptcha = {
   siteKey: process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY || "6LfzDdErAAAAAOdwh_1bxq9fTckoR7tnabeh6wyu", // client-side fallback
